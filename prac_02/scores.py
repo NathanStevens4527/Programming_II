17907312840febb8aa8787d4c752c52f9d4fc093
nathan <nathan.stevens.@my.jcu.edu.au>
2022-09-25 21:41:22 +1000

    add temperatures

---
+++ prac_02/scores.py
@@ -0,0 +1,16 @@
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
+
+def determine_result(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()
