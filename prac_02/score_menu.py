43ba8e65bb2f88283b90a739608ba9c377795e29
nathan <nathan.stevens.@my.jcu.edu.au>
2022-09-25 23:24:46 +1000

    add temperatures

---
+++ prac_02/score_menu.py
@@ -0,0 +1,32 @@
+MENU = "(P)rint stars\n(G)et result\n(Q)uit"
+
+def main():
+    score = int(input("Enter score: "))
+    print(MENU)
+    choice = input(">>> ")
+    while choice != "Q":
+        if choice == "P":
+            print_stars(score)
+        elif choice == "G":
+            result = determine_result(score)
+            print(result)
+        else:
+            print("Invalid choice")
+        choice = input("\n>>> ")
+    print("Goodbye")
+
+def determine_result(score):
+    if score > 100 or score < 0:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+def print_stars(score):
+    for i in range(0, score):
+        print("*", sep="", end="")
+
+main()
